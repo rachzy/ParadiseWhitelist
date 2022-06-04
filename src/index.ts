ba485820c49e361bdab0bf9c2e9da7c430be2a38
Henrique <rachultimate@gmail.com>
2022-06-04 11:37:08 -0300

    Add DiscordAPI and start Index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hiasda")
+import { Message, Client, Intents } from "discord.js";
+
+const { prefix, token } = require("../config.json");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once("ready", () => {
+  console.log("Bot is online");
+});
+
+client.on("messageCreate", async (message: Message) => {
+  const { author, content } = message;
+  if (author.bot || !content.startsWith(prefix)) return;
+
+  const command = content.replace(prefix, "");
+  if (command === "ping") {
+    message.channel.send("Pong!");
+  }
+});
+
+client.login(token);
